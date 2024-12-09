36cedc5dfe4b620669261433542801a93ab42bf9
AlexP2508 <alexander@pumarin.se>
2024-12-09 19:23:38 +0100

    Add Sign Up button to Header component with routing support

--- frontend/EE-movies-v1-react/src/App.tsx
+++ frontend/EE-movies-v1-react/src/App.tsx
@@ -1,10 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+
+const Login = () => <h1>Login Page</h1>;
+const SignUp = () => <h1>Sign Up Page</h1>;
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
     </div>
---
+++ frontend/EE-movies-v1-react/src/components/header/Header.tsx
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand as={NavLink} to="/" style={{ color: "gold" }}>
+          <FontAwesomeIcon icon={faVideoSlash} />
+          EA Movies
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          ></Nav>
+          <div>
+            <Button
+              variant="outline-info"
+              className="me-2"
+              as={NavLink}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button variant="outline-warning" as={NavLink} to="/signup">
+              Sign Up
+            </Button>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
