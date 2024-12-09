ee5d373bdd854d43380fdf0d13ad4bc07de0d24f
AlexP2508 <alexander@pumarin.se>
2024-12-09 19:07:26 +0100

    Finalize routing setup in main.tsx

--- frontend/EE-movies-v1-react/src/App.tsx
+++ frontend/EE-movies-v1-react/src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- frontend/EE-movies-v1-react/src/main.tsx
+++ frontend/EE-movies-v1-react/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+} else {
+  console.error("Root element not found!");
+}
